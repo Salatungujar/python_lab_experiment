e9f99bc7ea8516f2f700abba6190329ebf35467d
Salatungujar <162285077+Salatungujar@users.noreply.github.com>
2026-03-25 20:07:47 +0530

    Create addition_division.py

---
+++ addition_division.py
@@ -0,0 +1,15 @@
+#Additon
+num1 = float(input("Enter first no. of addition : "))
+num2 = float(input("Enter second no. of addition : "))
+sum_result = num1 + num2
+print(f"Sum : {num1} + {num2} = {sum_result}")
+
+#Division
+num3 = float(input("Enter the dividend for division : "))
+num4 = float(input("Enter the dividend for division : "))
+if (num4 == 0):
+    print("Error : Division is not allowed")
+else:
+    div_result = num3/num4
+    print(f"Divison : {num3}/{num4} = {div_result}")
+

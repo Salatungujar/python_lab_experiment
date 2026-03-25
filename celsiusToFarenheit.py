2eff709949fe0d141ece3ecc323a6ca26f926da5
Salatungujar <162285077+Salatungujar@users.noreply.github.com>
2026-03-25 22:27:08 +0530

    Add Celsius to Fahrenheit conversion script

---
+++ celsiusToFarenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter temperature in celsius : "))
+#conversion formula : farenheit = (celsius*9/5) + 32
+
+farenheit = (celsius * 9/5) + 32
+print(f"{celsius} degree celsius is equal to {farenheit} degrees farenheit")

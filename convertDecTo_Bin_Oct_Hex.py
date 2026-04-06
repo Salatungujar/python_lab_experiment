4089a5b117f3f8b90053f412deb13b2b9dcdc767
Salatungujar <162285077+Salatungujar@users.noreply.github.com>
2026-04-07 00:31:20 +0530

    Add decimal to binary, octal, and hexadecimal converter
    Implement conversion of decimal to binary, octal, and hexadecimal.

---
+++ convertDecTo_Bin_Oct_Hex.py
@@ -0,0 +1,9 @@
+#covert decimal to binary , octal and hexadecimal
+
+dec_num = int(input("Enter a decimal number :"))
+
+print("The decimal value of ", dec_num , "is :")
+
+print("Binary :", bin(dec_num))
+print("Octal :", oct(dec_num))
+print("Hexadecimal :", hex(dec_num))
